7957b2cee6372cd31a5604076521fa62fdf17be6
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-02-11 10:23:54 -0800

    fix: c-show-more.js can create undesirable tag (#441)
    * fix: c-show-more.js can create invalid tag
    
    * build: c-show-more.css

--- src/lib/_imports/components/c-show-more/c-show-more.js
+++ src/lib/_imports/components/c-show-more/c-show-more.js
@@ -5,9 +5,11 @@ export const DEFAULT_ELEMENTS = document.querySelectorAll(`
 /**
  * Convert elements with js-show-more--* classes into show-more components
  */
-export function generateMarkup(elements = DEFAULT_ELEMENTS) {
+export function generateMarkup(elements = DEFAULT_ELEMENTS, options = {
+  targetTag: undefined,
+}) {
   [...elements].forEach((element, index) => {
-    const wrapper = document.createElement('div');
+    const wrapper = document.createElement(element.tagName);
     wrapper.className = element.className.replace('js-show-more--', 'c-show-more c-show-more--');
 
     const checkbox = document.createElement('input');
@@ -15,7 +17,18 @@ export function generateMarkup(elements = DEFAULT_ELEMENTS) {
     checkbox.className = 'c-show-more__state';
     checkbox.id = `show-more-${index}`;
 
-    const target = document.createElement(element.tagName);
+    let targetTag = options.targetTag;
+    if (!targetTag) {
+      switch (true) {
+        case wrapper.className.includes('--one-line'):
+          targetTag = 'span';
+          break;
+        default:
+          targetTag = 'div';
+      }
+    }
+
+    const target = document.createElement(targetTag);
     target.className = 'c-show-more__target';
     target.innerHTML = element.innerHTML;
 
