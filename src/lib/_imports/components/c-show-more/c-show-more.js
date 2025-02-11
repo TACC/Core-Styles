26d065a999508bae46a8d737557720d98923b67e
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-02-11 16:19:40 -0600

    fix: c-show-more.js bugs

--- src/lib/_imports/components/c-show-more/c-show-more.js
+++ src/lib/_imports/components/c-show-more/c-show-more.js
@@ -1,44 +1,65 @@
-export const DEFAULT_ELEMENTS = document.querySelectorAll(`
-  [class*="js-show-more--"]
-`);
+export const defaults = {
+  elements: document.querySelectorAll(`
+    [class*="js-show-more"]
+  `),
+  options: {
+    lines: 1,
+    targetTag: 'span',
+    shouldToggleResembleLink: false,
+    startIndex: 0,
+  },
+};
 
 /**
  * Convert elements with js-show-more--* classes into show-more components
  */
-export function generateMarkup(elements = DEFAULT_ELEMENTS, options = {
-  targetTag: undefined,
-}) {
+export function generateMarkup(
+  elements = defaults.elements,
+  options = defaults.options
+) {
+  options = { ...defaults.options, ...options };
+
   [...elements].forEach((element, index) => {
+    if (element.querySelector('p, div, section, article, aside, nav, header, hgroup, footer, main')) {
+      console.warn(
+        'A "c-show-more" component expects only text and inline elements. Block elements may not truncate correctly. Consider restructuring content to avoid block elements.',
+        element
+      );
+    }
+
     const wrapper = document.createElement(element.tagName);
-    wrapper.className = element.className.replace('js-show-more--', 'c-show-more c-show-more--');
+    wrapper.className = element.className;
+    wrapper.classList.add('c-show-more');
+    wrapper.classList.add(`c-show-more--${options.lines === 1 ? 'one-line' : 'many-lines'}`);
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.className = 'c-show-more__state';
-    checkbox.id = `show-more-${index}`;
+    checkbox.id = `show-more-${options.startIndex + index}`;
 
-    let targetTag = options.targetTag;
-    if (!targetTag) {
-      switch (true) {
-        case wrapper.className.includes('--one-line'):
-          targetTag = 'span';
-          break;
-        default:
-          targetTag = 'div';
-      }
-    }
-
-    const target = document.createElement(targetTag);
+    const target = document.createElement(options.targetTag);
     target.className = 'c-show-more__target';
     target.innerHTML = element.innerHTML;
+    target.style.setProperty('--lines', options.lines);
 
     const toggle = document.createElement('label');
     toggle.className = 'c-show-more__toggle';
     toggle.htmlFor = checkbox.id;
-    toggle.innerHTML = `
-      <span class="c-show-more__on-text">Show More</span>
-      <span class="c-show-more__off-text">Show Less</span>
-    `;
+
+    const onText = document.createElement('span');
+    onText.className =
+      `${options.shouldToggleResembleLink ? 'x-link ' : ''}`
+      + 'c-show-more__on-text';
+    onText.textContent = 'Show More';
+
+    const offText = document.createElement('span');
+    offText.className =
+      `${options.shouldToggleResembleLink ? 'x-link ' : ''}`
+      + 'c-show-more__off-text';
+    offText.textContent = 'Show Less';
+
+    toggle.appendChild(onText);
+    toggle.appendChild(offText);
 
     wrapper.appendChild(checkbox);
     wrapper.appendChild(target);
